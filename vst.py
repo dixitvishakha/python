a96acf80c589ad4f6b30a2fd1b6905953e75ba72
dixitvishakha <vishakha_dixit@srmuniv.edu.in>
2017-07-15 15:04:44 +0530

    Add files via upload

---
+++ vst.py
@@ -0,0 +1,6 @@
+s=raw_input()
+c=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+if s in c:
+	print "Alphabet"
+else:
+	print "Not Alphabet"
